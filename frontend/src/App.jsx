2dd0252afeafb85be073c6ed697091b3d291c3ce
Emir Yavuz - Lenova <yvz.emir13@gmail.com>
2025-01-24 02:40:28 +0300

    Pages dosyaları oluşturuldu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Home from './pages/Home.jsx'
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Home></Home>
     </div>
   )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
         <h1 className="text-3xl font-bold underline">
-    Hello world!
+    Home
   </h1>
     </div>
   )
